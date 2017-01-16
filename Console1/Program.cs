c1bf9f3f534cf79af6e1b45ee0a4aaf3bcdb5902
https://github.com/KorSasha/CalcTest001.git <aleks.krot92@mail.ru>
2017-01-16 11:38:00 +0400

    Первая версия

---
+++ Console1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calc = new Calc();
+            int result = calc.Sum(1,2);
+            Console.WriteLine(string.Format(" result = {0}",result));
+           // Console.WriteLine($"(result)");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Console1;
+
+
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    ///<summary>
+    ///тестирование Calc
+    ///</summary>
+    public class UnitTest1
+    {
+        [TestMethod]
+
+        public void TestMethod1()
+        {
+            var calc = new Calc();
+            var result = calc.Sum(1, 2);
+            Assert.AreEqual(result, 3);
+
+        }
+    }
+}
